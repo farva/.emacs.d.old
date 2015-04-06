457a6873b3abe5e98081658da04627f87cfd5caa
farva <farva@a.baby>
2015-04-06 23:21:39 +0300

    company modes moved outside customize

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -11,6 +11,8 @@
         (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
         (add-to-list 'company-backends 'company-c-headers))))
 
+(setq company-global-modes
+      '(c-mode c++-mode emacs-lisp-mode makefile-mode makefile-gmake-mode))
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'init-company)
