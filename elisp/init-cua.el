62a6fef9e27c62fada483fb60f173d096bbe50a7
farva <farva@a.baby>
2015-04-07 09:31:40 +0300

    fix cua-mode

--- elisp/init-cua.el
+++ elisp/init-cua.el
@@ -1,3 +1,12 @@
-(cua-mode t nil (cua-base))
+;; enable CUA just for what I want
+(cua-selection-mode t)
+
+;;(setq cua-enable-cua-keys nil)
+;;(setq cua-highlight-region-shift-only t) ;; no transient mark mode
+;;(setq cua-toggle-set-mark nil) ;; original set-mark behavior, i.e. no transient-mark-mode
+;;(cua-mode)
+
+(define-key cua-global-keymap (kbd "C-<return>") nil)
+(define-key cua-global-keymap (kbd "C-c C-SPC") 'cua-set-rectangle-mark)
 
 (provide 'init-cua)
